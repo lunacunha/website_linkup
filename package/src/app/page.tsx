ac6a771c24936705f45fc8be8cfa7566bb9e0d0f
Luna <lunagomescunha@gmail.com>
2026-03-22 17:21:38 +0000

    improve packs

--- package/src/app/page.tsx
+++ package/src/app/page.tsx
@@ -4,7 +4,6 @@ import Aboutus from '@/app/components/Home/AboutUs'
 import Digital from '@/app/components/Home/Digital'
 import Beliefs from '@/app/components/Home/Beliefs'
 import Work from '@/app/components/Home/Work'
-import Team from '@/app/components/Home/Team'
 import Featured from '@/app/components/Home/Featured'
 import Manage from '@/app/components/Home/Manage'
 import FAQ from '@/app/components/Home/FAQ'
@@ -25,7 +24,6 @@ export default function Home() {
       <Digital />
       <Beliefs />
       <Work />
-      <Team />
       <Featured />
       <Manage />
       <FAQ />
